00985ca7995e297b426cb328a5cb5ed0ea024e19
nayan <nayansharma1010@gmail.com>
2023-03-14 12:14:56 +0530

    feat: post complete

--- src/task_list/models.rs
+++ src/task_list/models.rs
@@ -1,9 +1,13 @@
-use serde::Deserialize;
+use serde::{Serialize,Deserialize};
 
+#[derive(Serialize)]
+pub struct TaskList {
+    pub id:u32,
+    pub title:String,
+    pub contents:String
+}
 #[derive(Deserialize)]
 pub struct CreateTaskList {
-    pub title: String,
-    pub contents: String,
-    pub date: i64,
-    pub time: i64,
+    pub title:String,
+    pub contents:String
 }
--- src/task_list/services.rs
+++ src/task_list/services.rs
@@ -1,21 +1,13 @@
 use actix_web::{get,post,web,Responder,HttpResponse};
-use serde::{Serialize};
-use super::models::{CreateTaskList};
+use super::models::{TaskList,CreateTaskList};
 use crate::AppState;
 use std::process::exit;
-
-#[derive(Serialize)]
-struct TaskList {
-    id: u32,
-    title: String,
-    content: String,
-}
 
 
 #[get("/task_list")]
 async fn get_task_list(data: web::Data<AppState>) -> impl Responder {
     let pool = &data.pool;
-    let task_list = sqlx::query_as::<_,(u32,String,String)>("SELECT id,title,contents FROM task_list")
+    let task_list = sqlx::query_as::<_,(u32,String,String)>("SELECT * FROM task_list")
         .fetch_all(&*pool)
         .await;
     let data = match task_list {
@@ -27,13 +19,22 @@ async fn get_task_list(data: web::Data<AppState>) -> impl Responder {
         task_pool.push(TaskList {
             id: task.0,
             title:task.1,
-            content: task.2
+            contents: task.2
         });
     }
 
     return HttpResponse::Ok().json(task_pool);
-}   
+} 
+
+#[post("/task_list")]
+async fn post_task_list(data: web::Data<AppState>,param_obj: web::Json<CreateTaskList>) -> impl Responder {
+    let pool = &data.pool;
+    let query: String = format!("INSERT INTO task_list (title,contents) VALUES ('{}','{}')",param_obj.title.to_string(),param_obj.contents.to_string());
+    sqlx::query(&query).execute(&*pool).await.expect("there is an error");
+    return HttpResponse::Ok().json("Ok");
+}
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(get_task_list);
+    cfg.service(get_task_list)
+       .service(post_task_list);
 } 
